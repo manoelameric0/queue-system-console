da2c117ff48e1d6754fac6dcc0f701b2bd3fb06e
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-03-02 15:33:08 -0300

    Primeiro Teste executado com sucesso!
    teste do metodo Add de QueueService.
    Sucess

---
+++ QueueManagementSystem.Tests/QueueServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using QueueManagementSystem.Console;
+using QueueManagementSystem.Console.Enums;
+using QueueManagementSystem.Console.Models;
+using QueueManagementSystem.Console.Services;
+namespace QueueManagementSystem.Tests;
+public class QueueServiceTests
+{
+    [Fact]
+    public void AddClient_Should_Add_Client_To_Queue()
+    {   //Arrange → preparar cenário
+        var service = new QueueService();
+        //Act → executar ação
+        service.Add("Manoel", ClientType.Comum);
+        //Assert → verificar resultado
+        var Clients = service.GetClients();
+        Assert.Single(Clients);
+    }
+}
