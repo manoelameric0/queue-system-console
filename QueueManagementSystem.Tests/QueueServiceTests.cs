72940de479107aaa59de35cfd55ac274cb194f83
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-03-02 21:29:06 -0300

    Adicionando 2 testes para Add
    AddClient_Should_Throw_Exception_When_Name_Is_Empty: teste para ver se o nome é vazio ou espaços em branco e retorna a excessão valida.
    
    AddClient_Should_Throw_Exception_When_Name_Is_Short: teste para saber se o nome tem menos que 3 letras  e lança a excessão exata.

--- QueueManagementSystem.Tests/QueueServiceTests.cs
+++ QueueManagementSystem.Tests/QueueServiceTests.cs
@@ -3,29 +3,60 @@ using QueueManagementSystem.Console.Enums;
 using QueueManagementSystem.Console.Models;
 using QueueManagementSystem.Console.Services;
 namespace QueueManagementSystem.Tests;
+
 public class QueueServiceTests
 {
     [Fact]
     public void AddClient_Should_Add_Client_To_Queue()
     {   //Arrange → preparar cenário
         var service = new QueueService();
+
         //Act → executar ação
         service.Add("Manoel", ClientType.Comum);
+
         //Assert → verificar resultado
         var Clients = service.GetClients();
         Assert.Single(Clients);
     }
-    
+
     [Fact]
-        public void CallNext_Should_Call_Priority_After_Three_Normal()
+    public void AddClient_Should_Throw_Exception_When_Name_Is_Empty()
+    {
+        // Given
+        var service = new QueueService();
+
+
+        // When
+        var exeception = Assert.Throws<ArgumentException>(() => service.Add("", ClientType.Comum));
+
+        // Then
+        Assert.Equal("Nome inválido", exeception.Message);
+    }
+
+    [Fact]
+    public void AddClient_Should_Throw_Exception_When_Name_Is_Short()
+    {
+        // Given
+        var service = new QueueService();
+
+        // When
+        var exception = Assert.Throws<ArgumentException>(() => service.Add("Ma", ClientType.Prioridade));
+        
+
+        // Then
+        Assert.Equal("Nome inválido: mínimo 3 caracteres e sem números.", exception.Message);
+    }
+
+    [Fact]
+    public void CallNext_Should_Call_Priority_After_Three_Normal()
     {
         //Arrange → preparar cenário
         var service = new QueueService();
 
-        service.Add("abc", ClientType.Comum);
-        service.Add("efg", ClientType.Comum);
-        service.Add("hij", ClientType.Comum);
-        service.Add("kjl", ClientType.Prioridade);
+        service.Add("Manoel", ClientType.Comum);
+        service.Add("Andryelle", ClientType.Comum);
+        service.Add("Madry", ClientType.Comum);
+        service.Add("Manoelle", ClientType.Prioridade);
 
         //Act → executar ação
         service.CallNext();
@@ -36,7 +67,7 @@ public class QueueServiceTests
         //Assert → verificar resultado
         var historico = service.GetHistory();
 
-        Assert.Equal("kjl", historico.First().Name);
+        Assert.Equal("Manoelle", historico.First().Name);
     }
 
     [Fact]
