12614d90ca42ba4e1138d487e0700ceda6d73462
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-03-05 16:35:16 -0300

    Adciona ao Policy o teste para retornar prioridade quando o contador for 3

--- QueueManagementSystem.Tests/CallOrderPolicyTest.cs
+++ QueueManagementSystem.Tests/CallOrderPolicyTest.cs
@@ -17,4 +17,17 @@ public class CallOrderPolicyTest
         // Assert
         Assert.Equal("Normal", resultado);
     }
+
+    [Fact]
+    public void Policy_Should_Return_Priority_When_Counter_Is_Three()
+    {
+        // Arrange
+        var regra = new CallOrderPolicy();
+
+        // Act
+        var resultado = regra.Proximo(3, 2);
+
+        // Assert
+        Assert.Equal("Prioridade", resultado);
+    }
 }
