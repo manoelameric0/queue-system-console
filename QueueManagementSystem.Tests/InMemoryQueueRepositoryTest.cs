62d0c9d1c99adc645378dc24202f75c8aa4394c4
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-03-10 18:02:23 -0300

    Cria o Arquivo de test para o repository
    - testa se está adicionando Cliente na fila.
    - testa se está retornando a excessão de duplicidade.
    -testa se adiciona apenas 1 cliente depois da excessão.

---
+++ QueueManagementSystem.Tests/InMemoryQueueRepositoryTest.cs
@@ -0,0 +1,58 @@
+using System;
+using QueueManagementSystem.Console.Enums;
+using QueueManagementSystem.Console.Models;
+using QueueManagementSystem.Console.Repositories;
+using QueueManagementSystem.Console.Services;
+
+namespace QueueManagementSystem.Tests;
+
+public class InMemoryQueueRepositoryTest
+{
+    [Fact]
+    public void Add_Should_Add_Client_To_Repository()
+    {
+        // Arrange
+        var _repository = new InMemoryQueueRepository();
+
+
+        // Act
+        _repository.Add(new Client(name:"Manoel",clientType: Console.Enums.ClientType.Comum));
+
+        // Assert
+        var clients = Assert.Single(_repository.GetAll());
+
+        Assert.Equal("Manoel",clients.Name);
+    }
+
+    [Fact]
+    public void Add_Should_Throw_Exception_When_Client_Is_Duplicated()
+    {
+        // Arrange
+        var service = new QueueService();
+
+        service.Add("Manoel", Console.Enums.ClientType.Comum);
+
+        // Act
+        var exception = Assert.Throws<ArgumentException>(() => service.Add("Manoel", Console.Enums.ClientType.Comum));
+
+        // Assert
+        Assert.Equal("Cliente já está na Fila", exception.Message);
+    }
+
+    [Fact]
+    public void Add_Should_Not_Add_Client_When_Duplicate_Exception_Is_Thrown()
+    {
+        // Arrange
+        var repository = new InMemoryQueueRepository();
+        var service = new QueueService(repository);
+
+        service.Add("Manoel", ClientType.Comum);
+
+        // Act
+        var exception = Assert.Throws<ArgumentException>(() => service.Add("Manoel",ClientType.Comum));
+
+        // Assert
+        Assert.Single(repository.GetAll());
+    }
+
+}
