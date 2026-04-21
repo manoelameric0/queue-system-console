a5531fb1070529b1e89a838332cbc5c198ae9ae3
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-04-21 11:02:36 -0300

    Create: Controller do sistema

---
+++ QueueManagementSystem.API/Controllers/QueueController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QueueManagementSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QueueController : ControllerBase
+    {
+    }
+}
