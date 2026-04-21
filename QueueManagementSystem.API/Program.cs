44521c9778345aefa974c8df7e8c9c7d03d0bf67
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-04-21 11:01:19 -0300

    Adicionando a Injeção d service, repository e policy no program.cs da API

--- QueueManagementSystem.API/Program.cs
+++ QueueManagementSystem.API/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.Extensions.FileSystemGlobbing;
+using QueueManagementSystem.Core.Interfaces;
+using QueueManagementSystem.Core.Policies;
+using QueueManagementSystem.Core.Services;
+using QueueManagementSystem.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +11,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// service
+builder.Services.AddScoped<IQueueService, QueueService>();
+
+//Repository
+builder.Services.AddSingleton<IQueueRepository, InMemoryQueueRepository>();
+
+//Policy
+builder.Services.AddSingleton<ICallOrderPolicy, CallOrderPolicy>();
 
 var app = builder.Build();
 
