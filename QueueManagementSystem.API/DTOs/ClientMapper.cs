2fae23184e17a44f48ae09e6bd82709ee93868ea
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-04-22 18:30:33 -0300

    create: cria uma class para agilizar o SELECT dos clientes para o DTO de queueState

---
+++ QueueManagementSystem.API/DTOs/ClientMapper.cs
@@ -0,0 +1,21 @@
+using System;
+using QueueManagementSystem.Core.Models;
+
+namespace QueueManagementSystem.API.DTOs;
+
+public class ClientMapper
+{
+    public static ClientResponse MapClient(Client c)
+    {
+        return new ClientResponse
+        {
+            Name = c.Name,
+            ClientType = c.ClientType,
+            EnQueueTime = c.EnQueueTime,
+            CallTime = c.CallTime
+
+        };
+            
+        
+    }
+}
