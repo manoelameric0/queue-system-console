7e051cdee680293ba72d92d054ba9cd1b92c0ef7
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-04-22 18:31:45 -0300

    remove: remove o OpenApi do program.cs

--- QueueManagementSystem.API/Program.cs
+++ QueueManagementSystem.API/Program.cs
@@ -9,8 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
 // swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -29,7 +28,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
