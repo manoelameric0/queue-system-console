dcd0fc20190820bc415b629b06688bfc5121706a
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-04-26 10:26:45 -0300

    Adiciona QueueDbContext
    Inclui a classe QueueDbContext com DbSet<Client> para integração com EF Core

---
+++ QueueManagementSystem.Infrastructure/Data/QueueDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QueueManagementSystem.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueManagementSystem.Infrastructure.Data
+{
+    public class QueueDbContext : DbContext
+    {
+        // Construtor que recebe as opções de configuração
+        public QueueDbContext(DbContextOptions<QueueDbContext> contextOptions) : base (contextOptions)
+        {
+        }
+
+        // Definição das tabelas (DbSets)
+        public DbSet<Client> Clients { get; set; }
+    }
+}
