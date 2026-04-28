6d61be10ffe3cd67fcd7a942762fd6b210cea6f9
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-04-28 18:40:30 -0300

    Desabilita repositório em memória e adiciona QueueRepository
    Comentado o InMemoryQueueRepository.cs, desabilitando sua implementação. Adicionado QueueRepository.cs com implementação inicial do método Add utilizando o contexto de banco de dados QueueDbContext.

--- QueueManagementSystem.Infrastructure/Repositories/InMemoryQueueRepository.cs
+++ QueueManagementSystem.Infrastructure/Repositories/InMemoryQueueRepository.cs
@@ -1,31 +1,31 @@
-using System;
-using QueueManagementSystem.Core.Models;
-using QueueManagementSystem.Core.Interfaces;
+//using System;
+//using QueueManagementSystem.Core.Models;
+//using QueueManagementSystem.Core.Interfaces;
 
-namespace QueueManagementSystem.Infrastructure.Repositories;
+//namespace QueueManagementSystem.Infrastructure.Repositories;
 
-public class InMemoryQueueRepository:IQueueRepository
-{
-    private readonly List<Client> _clientQueue = new();
+//public class InMemoryQueueRepository:IQueueRepository
+//{
+//    private readonly List<Client> _clientQueue = new();
 
-    public void Add(Client client)
-    {
-        _clientQueue.Add(client);
-    }
+//    public void Add(Client client)
+//    {
+//        _clientQueue.Add(client);
+//    }
 
-    public void Remove(Client client) => _clientQueue.Remove(client);
+//    public void Remove(Client client) => _clientQueue.Remove(client);
 
-    public void RestoreClient(Client client) => Add(client);
+//    public void RestoreClient(Client client) => Add(client);
 
-    public IEnumerable<Client> GetAll()
-     => _clientQueue ?? Enumerable.Empty<Client>();
+//    public IEnumerable<Client> GetAll()
+//     => _clientQueue ?? Enumerable.Empty<Client>();
 
-    public bool Exists(string name)
-    {
-        bool exist = _clientQueue.Any(c => string.Equals(c.Name, name, StringComparison.OrdinalIgnoreCase));
-        return exist;
-    }
+//    public bool Exists(string name)
+//    {
+//        bool exist = _clientQueue.Any(c => string.Equals(c.Name, name, StringComparison.OrdinalIgnoreCase));
+//        return exist;
+//    }
 
 
     
-}
+//}
---
+++ QueueManagementSystem.Infrastructure/Repositories/QueueRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using QueueManagementSystem.Core;
+using QueueManagementSystem.Core.Interfaces;
+using QueueManagementSystem.Core.Models;
+using QueueManagementSystem.Infrastructure.Data;
+
+namespace QueueManagementSystem.Infrastructure.Repositories
+{
+    public class QueueRepository : IQueueRepository
+    {
+        private readonly QueueDbContext _dbContext;
+
+        public QueueRepository(QueueDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public async Task Add(Client client)
+        {
+            await _dbContext.AddAsync(client);
+            await _dbContext.SaveChangesAsync();
+        }
+
+
+    }
+}
