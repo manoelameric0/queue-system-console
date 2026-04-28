33f8ef1b7001b418e2a1deeb192db4fde321b382
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-04-28 12:01:07 -0300

    Cria migração inicial e tabela Clients no SQLite
    Adiciona arquivos de migração do EF Core para criar a tabela "Clients" com as colunas ID, Name, Type, QueuedAt e CalledAt. Inclui snapshot do modelo e arquivo do banco de dados SQLite.

---
+++ QueueManagementSystem.Infrastructure/Migrations/20260428145212_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QueueManagementSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Type = table.Column<int>(type: "INTEGER", nullable: false),
+                    QueuedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CalledAt = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
