dd6093cde2ba916ea7a9ca982b3c49f37ed50d11
Manoel Américo <manoelsilva.pro.mcz@hotmail.com>
2026-05-02 10:43:48 -0300

    refact: atualiza o inMemori para o console usar ele normalmente com async

--- QueueManagementSystem.Infrastructure/Repositories/InMemoryQueueRepository.cs
+++ QueueManagementSystem.Infrastructure/Repositories/InMemoryQueueRepository.cs
@@ -1,31 +1,36 @@
-//using System;
-//using QueueManagementSystem.Core.Models;
-//using QueueManagementSystem.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using QueueManagementSystem.Core.Interfaces;
+using QueueManagementSystem.Core.Models;
+using System;
 
-//namespace QueueManagementSystem.Infrastructure.Repositories;
+namespace QueueManagementSystem.Infrastructure.Repositories;
 
-//public class InMemoryQueueRepository:IQueueRepository
-//{
-//    private readonly List<Client> _clientQueue = new();
+public class InMemoryQueueRepository : IQueueRepository
+{
+    private readonly List<Client> _clientQueue = new();
 
-//    public void Add(Client client)
-//    {
-//        _clientQueue.Add(client);
-//    }
+    public async Task Add(Client client)
+    {
+        _clientQueue.Add(client);
+    }
 
-//    public void Remove(Client client) => _clientQueue.Remove(client);
+    public async Task Remove(Client client) => _clientQueue.Remove(client);
 
-//    public void RestoreClient(Client client) => Add(client);
+    public async Task<IEnumerable<Client>> GetQueue()
+     => _clientQueue.Where(c => c.CalledAt == null).OrderBy(c => c.QueuedAt).ToList() ?? Enumerable.Empty<Client>();
+    public async Task<IEnumerable<Client>> GetHistory()
+    => _clientQueue.Where(c => c.CalledAt != null).OrderBy(c => c.QueuedAt).ToList() ?? Enumerable.Empty<Client>();
 
-//    public IEnumerable<Client> GetAll()
-//     => _clientQueue ?? Enumerable.Empty<Client>();
+    public async Task<bool> Exists(string name) => _clientQueue.Any(c => string.Equals(c.Name, name, StringComparison.OrdinalIgnoreCase));
 
-//    public bool Exists(string name)
-//    {
-//        bool exist = _clientQueue.Any(c => string.Equals(c.Name, name, StringComparison.OrdinalIgnoreCase));
-//        return exist;
-//    }
+    public async Task<bool> HasHistory() =>  _clientQueue.Any(c => c.CalledAt != null);
+    public async Task<bool> HasClients() => _clientQueue.Any(c => c.CalledAt == null);
+
+    public async Task SaveChanges()
+    {
+
+    }
 
 
-    
-//}
+
+}
